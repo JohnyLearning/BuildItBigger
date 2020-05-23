41f698c69ea945392a20e4520dd8c50f67878470
Ivan Hadzhi <ivan.hadzhiiliev@scotiabank.com>
2020-05-23 16:02:23 -0400

    config: setup GCE + add dependecy for Joke tell library to backend

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,6 +3,7 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.ihadzhi.jokerlib.JokeTeller;
 
 import javax.inject.Named;
 
@@ -22,8 +23,8 @@ public class MyEndpoint {
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
-
+        StringBuilder builder = new StringBuilder("Hi, ").append(name).append("<br/>").append(JokeTeller.tellJoke());
+        response.setData(builder.toString());
         return response;
     }
 
