5036a2b83c71654f006202414aaef749729073e4
Ivan Hadzhi <ivan.hadzhiiliev@scotiabank.com>
2020-05-24 23:55:29 -0400

    test: Added async task test config: add paid and free flavors and remove ads from paid.

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -0,0 +1,35 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Context;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Pair;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(AndroidJUnit4.class)
+public class EndpointsAsyncTaskTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testBackgroundJoke() throws Exception {
+        final CountDownLatch signal = new CountDownLatch(1);
+        new EndpointsAsyncTask(new EndpointsAsyncTask.Callback() {
+            @Override
+            public void execute(String joke) {
+                assertNotNull(joke);
+                signal.countDown();
+            }
+        }).execute(new Pair<Context, String>(activityTestRule.getActivity(), "Nobody"));
+        signal.await();
+    }
+
+}
